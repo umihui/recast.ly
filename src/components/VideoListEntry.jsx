41cb13be313a76169db0264ffe3766f5c333003c
Hack Reactor Student <student@hackreactor.com>
2017-09-08 21:05:23 -0700

    passed App tests

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,18 +1,31 @@
-var VideoListEntry = (props) => {
-  //console.log(props.video.snippet);
+class VideoListEntry extends React.Component {
 
-  return (
-  <div className="video-list-entry media">
-    <div className="media-left media-middle">
-      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
-    </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">{props.video.snippet.title}</div>
-      <div className="video-list-entry-detail">{props.video.snippet.description}</div>
-    </div>
-  </div>
+  constructor(props) {
+    super(props);
 
-  );
+    this.listenOnClick = this.listenOnClick.bind(this);
+  }
+
+  listenOnClick(e){
+    console.log(e);
+    this.props.newclickEvent(e.video);
+  }
+
+  render(){
+    return (
+      <div className="video-list-entry media">
+        <div className="media-left media-middle">
+          <img className="media-object" src={this.props.video.snippet.thumbnails.default.url} alt="" />
+        </div>
+        <div className="media-body">
+          <div className="video-list-entry-title" onClick={this.listenOnClick.bind(this,this.props)}>{this.props.video.snippet.title}</div>
+          <div className="video-list-entry-detail">{this.props.video.snippet.description}</div>
+        </div>
+      </div>
+
+    );
+  }
+
 };
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
