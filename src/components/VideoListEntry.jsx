1a95a261ad2503a17eac3b659bc4c7b25951cd61
Hack Reactor Student <student@hackreactor.com>
2017-09-08 21:43:34 -0700

    pass all tests up to API

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,32 +1,32 @@
-class VideoListEntry extends React.Component {
+var VideoListEntry = (props) => (
 
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
+  //
+  //   //this.listenOnClick = this.listenOnClick.bind(this);
+  // }
 
-    this.listenOnClick = this.listenOnClick.bind(this);
-  }
+  // listenOnClick(e){
+  //   console.log(e);
+  //   this.props.newclickEvent(e.video);
+  // }
 
-  listenOnClick(e){
-    console.log(e);
-    this.props.newclickEvent(e.video);
-  }
 
-  render(){
-    return (
+  //  return (
       <div className="video-list-entry media">
         <div className="media-left media-middle">
-          <img className="media-object" src={this.props.video.snippet.thumbnails.default.url} alt="" />
+          <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
         </div>
         <div className="media-body">
-          <div className="video-list-entry-title" onClick={this.listenOnClick.bind(this,this.props)}>{this.props.video.snippet.title}</div>
-          <div className="video-list-entry-detail">{this.props.video.snippet.description}</div>
+
+          <div className="video-list-entry-title" onClick={() => props.newclickEvent(props.video)}>{props.video.snippet.title}</div>
+          <div className="video-list-entry-detail">{props.video.snippet.description}</div>
         </div>
       </div>
 
     );
-  }
 
-};
+//<div className="video-list-entry-title" onClick={this.listenOnClick.bind(this,this.props)}>{this.props.video.snippet.title}</div>
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoListEntry.propTypes = {
